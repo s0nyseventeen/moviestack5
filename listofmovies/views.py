6949aa0e66ad143848074f026fa2e353c33269c0
s0nyseventeen <sonysevent@gmail.com>
2020-11-17 18:09:12 +0200

    movie-detail html ready

--- listofmovies/views.py
+++ listofmovies/views.py
@@ -7,18 +7,19 @@ def home(request):
 def list_of_movies(request):
     movies = Movie.objects.all()
     directors = Director.objects.all()
-    # genres = Genre.objects.all()
 
     return render(
         request,
         'listofmovies.html',
-        context = {
-            'movies': movies,
-            'directors': directors,
-            # 'genres': genres,
-        }
+        context = {'movies': movies, 'directors': directors}
     )
 
 def movie_details(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, 'movie_detail.html', {'movie': movie})
+    movie_genre = Movie.objects.all()
+
+    return render(
+        request,
+        'movie_detail.html',
+        {'movie': movie, 'movie_genre': movie_genre}
+    )
