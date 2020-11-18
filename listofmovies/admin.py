22f2bc6a6a6d3289fb97b1436087ce730c115dea
s0nyseventeen <sonysevent@gmail.com>
2020-11-18 15:40:09 +0200

    admin config

--- listofmovies/admin.py
+++ listofmovies/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Movie, Genre, Director
 
-admin.site.register(Movie)
 admin.site.register(Genre)
-admin.site.register(Director)
+
+
+class DirectorInline(admin.TabularInline):
+
+    model = Director
+
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'director', 'show_genre', 'release')
+    list_filter = ('release', 'genre')
+    inlines = [DirectorInline]
--- listofmovies/models.py
+++ listofmovies/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-
 class Genre(models.Model):
 
 	name = models.CharField(max_length=200, help_text='Enter a movie genre')
@@ -39,14 +38,16 @@ class Movie(models.Model):
 	def get_summary(self):
 		return self.description[:100] + '...'
 
+	def show_genre(self):
+		return ', '.join([ genre.name for genre in self.genre.all()[:3]] )
+
+	show_genre.short_description = 'Genre'
+
 
 class Director(models.Model):
 
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
-
-	# def get_absolute_url(self):
-	# 	return reverse('director-detail', args=[str(self.id)])
 
 	def __str__(self):
 		return f'{self.first_name} {self.last_name}'
